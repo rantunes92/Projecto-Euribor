0045d50ddee83698d825278820e4d365c091d605
rantunes92 <r.jantunes-25@hotmail.com>
2025-08-06 11:04:05 +0100

    Update ProjectoEuribor.py

--- ProjectoEuribor.py
+++ ProjectoEuribor.py
@@ -100,7 +100,7 @@ print(MediaEuribor)
 ValorRemanescente=199742.30
 PrestacaoFaltam=431
 
-Ago25=[2,070, 2.077, 2.075]
+Ago25=[2,070, 2.077, 2.075, 2.089]
 MediaEuribor=round(np.mean(Ago25), 3)
 print(MediaEuribor)
 ValorRemanescente=199742.30
@@ -125,5 +125,6 @@ plt.legend()
 plt.grid(True)
 plt.show()
 """
+
 
 
