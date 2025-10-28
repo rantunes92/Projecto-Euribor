8bfa1c51e971e05101686c2a3d3c2c8612715677
rantunes92 <r.jantunes-25@hotmail.com>
2025-10-28 11:12:50 +0000

    Refactor ProjectoEuribor.py by removing comments

--- ProjectoEuribor.py
+++ ProjectoEuribor.py
@@ -123,7 +123,7 @@ ValorRemanescente=199258.52
 PrestacaoFaltam=429
 
 Out25=[2.079, 2.083, 2.095, 2.103, 2.106, 2.103, 2.104, 2.099, 2.103, 2.106, 2.103, 2.110, 2.113, 2.100, 2.110, 2.099, 2.099,
-      2.104, 2.124]
+      2.104, 2.124, 2.126]
 MediaEuribor=round(np.mean(Out25), 3)
 print(MediaEuribor)
 ValorRemanescente=199015.63
@@ -151,6 +151,7 @@ plt.legend()
 plt.grid(True)
 plt.show()
 """
+
 
 
 
