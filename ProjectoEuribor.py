ad816b23b35fcf55080339928abd127ec63ed5f6
rantunes92 <r.jantunes-25@hotmail.com>
2026-07-08 11:09:07 +0100

    Update ProjectoEuribor.py

--- ProjectoEuribor.py
+++ ProjectoEuribor.py
@@ -194,7 +194,7 @@ print(MediaEuribor)
 ValorRemanescente=191861.48
 PrestacaoFaltam=420
 
-Jul26=[2.554, 2.565, 2.554, 2.547, 2.542]
+Jul26=[2.554, 2.565, 2.554, 2.547, 2.542, 2.567]
 MediaEuribor=round(np.mean(Jul26), 3)
 print(MediaEuribor)
 ValorRemanescente=191861.48
